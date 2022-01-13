f1642261cc20b0d4f777e036a4b4af922aa5c0e6
ma0048 <ma0048@technikum-wien.at>
2022-01-13 11:46:42 +0100

    http code geandert

--- controllers/rest/v1/Cards.php
+++ controllers/rest/v1/Cards.php
@@ -47,29 +47,29 @@ class Cards extends API_Controller
 		$cardIdentifier = $this->_ci->get('cardIdentifier');
 
 		if (is_null($cardIdentifier))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehlerhafte Parameterübergabe'), REST_Controller::HTTP_BAD_REQUEST);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehlerhafte Parameterübergabe'), REST_Controller::HTTP_OK);
 
 		$card = $this->_ci->BetriebsmittelpersonModel->getBetriebsmittelZuordnung($cardIdentifier);
 
 		if (!hasData($card))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Konnte Karte keiner Person zuweisen. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Konnte Karte keiner Person zuweisen. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$cardUser = getData($card)[0]->uid;
 
 		$user = $this->_ci->BenutzerModel->load(array('uid' => $cardUser));
 
 		if (!hasData($user))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$uid = getData($user)[0]->uid;
 
 		$studiengang = $this->_ci->KontoModel->getLastStudienbeitrag($uid, implode("','" , $this->_ci->config->item('BUCHUNGSTYPEN')));
 
 		if (isError($studiengang))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Auslesen des Studienganges. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Auslesen des Studienganges. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		if (!hasData($studiengang))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Verlängerung der Karte ist derzeit nicht möglich da der Studienbeitrag noch nicht bezahlt wurde.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Verlängerung der Karte ist derzeit nicht möglich da der Studienbeitrag noch nicht bezahlt wurde.'), REST_Controller::HTTP_OK);
 
 		$studiensemester_kurzbz = getData($studiengang)[0]->studiensemester_kurzbz;
 
@@ -86,19 +86,19 @@ class Cards extends API_Controller
 		$type = $this->_ci->get('type');
 
 		if (is_null($hash) || is_null($pin) || is_null($type))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehlerhafte Parameterübergabe'), REST_Controller::HTTP_BAD_REQUEST);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehlerhafte Parameterübergabe'), REST_Controller::HTTP_OK);
 
 		$user = $this->_ci->CardModel->loadWhere(array('zugangscode' => $hash, 'pin' => $pin));
 
 		if (!hasData($user))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$uid = getData($user)[0]->uid;
 
 		$person = $this->_ci->PersonModel->getByUid($uid);
 
 		if (!hasData($person))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$person = getData($person)[0];
 
@@ -110,32 +110,32 @@ class Cards extends API_Controller
 		$uid = $this->_ci->get('uid');
 
 		if (is_null($uid))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehlerhafte Parameterübergabe'), REST_Controller::HTTP_BAD_REQUEST);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehlerhafte Parameterübergabe'), REST_Controller::HTTP_OK);
 
 		$person = $this->_ci->PersonModel->getByUid($uid);
 
 		if (!hasData($person))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$person = getData($person)[0];
 
 		$personFoto = $this->_ci->FotostatusPersonModel->getLastFotoStatus($person->person_id);
 
 		if (!hasData($personFoto))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person hat kein Foto. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person hat kein Foto. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$personFoto = getData($personFoto)[0];
 
 		if ($personFoto->fotostatus_kurzbz === 'abgewiesen')
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Das Foto wurde abgewiesen. Laden Sie bitte ein gültiges Foto hoch.'), REST_Controller::HTTP_FORBIDDEN);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Das Foto wurde abgewiesen. Laden Sie bitte ein gültiges Foto hoch.'), REST_Controller::HTTP_OK);
 
 		if ($personFoto->fotostatus_kurzbz === 'hochgeladen')
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Foto wurde noch nicht akzeptiert.'), REST_Controller::HTTP_FORBIDDEN);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Foto wurde noch nicht akzeptiert.'), REST_Controller::HTTP_OK);
 
 		if ($personFoto->fotostatus_kurzbz === 'akzeptiert')
 			$this->_ci->response(array('photo' => $person->foto, 'error' => null), REST_Controller::HTTP_OK);
 		else
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Laden Sie bitte ein gültiges Foto hoch.'), REST_Controller::HTTP_FORBIDDEN);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Laden Sie bitte ein gültiges Foto hoch.'), REST_Controller::HTTP_OK);
 
 	}
 	/**
@@ -147,12 +147,12 @@ class Cards extends API_Controller
 		$photo = $this->_ci->post('photo');
 
 		if (is_null($uid) || is_null($photo))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehlerhafte Parameterübergabe'), REST_Controller::HTTP_BAD_REQUEST);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehlerhafte Parameterübergabe'), REST_Controller::HTTP_OK);
 
 		$person = $this->_ci->PersonModel->getByUid($uid);
 
 		if (!hasData($person))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$person = getData($person)[0];
 
@@ -170,12 +170,12 @@ class Cards extends API_Controller
 		$cardData = $this->_ci->post('cardIndetifier');
 
 		if (is_null($uid) || is_null($cardData))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehlerhafte Parameterübergabe'), REST_Controller::HTTP_BAD_REQUEST);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehlerhafte Parameterübergabe'), REST_Controller::HTTP_OK);
 
 		$person = $this->_ci->PersonModel->getByUid($uid);
 
 		if (!hasData($person))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Person kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$person = getData($person)[0];
 
@@ -193,7 +193,7 @@ class Cards extends API_Controller
 		);
 
 		if (isError($insert))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Speichern des Betriebsmittels. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Speichern des Betriebsmittels. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$this->_ci->BetriebsmittelpersonModel->insert(
 			array(
@@ -207,7 +207,7 @@ class Cards extends API_Controller
 		);
 
 		if (isError($insert))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Speichern des Betriebsmittels. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Speichern des Betriebsmittels. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$qrCode = $this->_ci->CardModel->loadWhere(array('uid' => $uid));
 
@@ -226,7 +226,7 @@ class Cards extends API_Controller
 		$this->_uid = getAuthUID();
 
 		if (!$this->_uid)
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'User authentification failed.'), REST_Controller::HTTP_UNAUTHORIZED);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'User authentification failed.'), REST_Controller::HTTP_OK);
 	}
 
 	private function _addPhoto($photo, $person)
@@ -236,7 +236,7 @@ class Cards extends API_Controller
 		$exists = $this->_ci->AkteModel->getAkten($person->person_id, 'Lichtbil');
 
 		if (isError($exists))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Akte kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Die Akte kann nicht geladen werden. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$data = array(
 			'dokument_kurzbz' => 'Lichtbil',
@@ -272,7 +272,7 @@ class Cards extends API_Controller
 		}
 
 		if (isError($result))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Speichern der Akte. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Speichern der Akte. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$photo = resize($photo, 101, 130);
 
@@ -283,7 +283,7 @@ class Cards extends API_Controller
 		);
 
 		if (isError($result))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Speichern des Fotos. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Speichern des Fotos. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 
 		$insert = $this->_ci->FotostatusPersonModel->insert(
 			array(
@@ -298,6 +298,6 @@ class Cards extends API_Controller
 		);
 
 		if (isError($insert))
-			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Speichern des Fotostatus. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_NOT_FOUND);
+			$this->_ci->response(array('validdate' => 'CUSTOMERROR', 'error' => 'Fehler beim Speichern des Fotostatus. Bitte wenden Sie sich an den Service Desk.'), REST_Controller::HTTP_OK);
 	}
 }
