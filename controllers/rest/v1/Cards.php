aa2f889cac8aee528897828c1a700a3ffa8b3138
ma0048 <ma0048@technikum-wien.at>
2022-08-10 16:59:55 +0200

    titelpost wird mit nachname mitgeschickt

--- controllers/rest/v1/Cards.php
+++ controllers/rest/v1/Cards.php
@@ -134,7 +134,7 @@ class Cards extends API_Controller
 			$personData = array(
 				'uid' => $benutzer->uid,
 				'firstname' => $benutzer->vorname,
-				'lastname' => $benutzer->nachname,
+				'lastname' => $benutzer->nachname . ', ' . $benutzer->titelpost,
 				'titelpre' => $benutzer->titelpre,
 				'titelpost' => $benutzer->titelpost,
 				'degreeprogram' => $student->kurzbzlang,
